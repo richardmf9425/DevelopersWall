542c1876ecb5a3346a7f999d71979ab0f1d74083
Richard Mena <richardmf9425@yahoo.com>
2019-09-25 17:52:20 -0400

    add hashing and jwt auth

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = config.get('mongoURI');
 
 const connectMongoose = async () => {
 	try {
-		await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+		await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 	} catch (e) {
 		console.error(e.message);
 		process.exit(1);
