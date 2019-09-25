a7f7dfed7f6517d1add73d7337264e3a25de62ce
Richard Mena <richardmf9425@yahoo.com>
2019-09-25 16:55:48 -0400

    add user signup validation with express validator

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectMongoose = async () => {
+	try {
+		await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+	} catch (e) {
+		console.error(e.message);
+		process.exit(1);
+	}
+};
+
+module.exports = connectMongoose;
