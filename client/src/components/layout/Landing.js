bdb6990486e822d961e69b3a0d15cd9162ead38e
Richard Mena <richardmf9425@yahoo.com>
2019-10-05 19:47:13 -0400

    add about section in landing and skew design, might change later

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
 	return (
-		<section className="landing">
-			<div className="dark-overlay">
-				<div className="landing-inner">
-					<h1 className="x-large">Developers Wall</h1>
-					<p className="lead">Don't search for jobs. Let them search for you</p>
-					<div className="buttons">
-						<a href="register.html" className="btn btn-primary">
-							Sign Up
-						</a>
-						<a href="login.html" className="btn btn-light">
-							Log In
-						</a>
+		<Fragment>
+			<section className="landing">
+				<div className="dark-overlay">
+					<div className="landing-inner">
+						<h1 className="x-large">Developers Wall</h1>
+						<p className="lead">Don't search for jobs. Let them search for you</p>
+						<div className="buttons">
+							<Link to="/signup" className="btn btn-primary">
+								Sign Up
+							</Link>
+							<Link to="/login" className="btn btn-light">
+								Log In
+							</Link>
+						</div>
 					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+			<section className="about-section">
+				<div className="about-inner">
+					<h1 className="about-header">About</h1>
+					<div className="about-content">
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt libero aliquid corporis
+							modi odit, veniam officiis ducimus illo error, ad cupiditate dicta enim possimus, quaerat
+							accusantium quo provident quis commodi architecto explicabo sed eveniet. Culpa ea voluptatem
+							eveniet amet possimus?
+						</p>
+					</div>
+				</div>
+			</section>
+		</Fragment>
 	);
 };
 
