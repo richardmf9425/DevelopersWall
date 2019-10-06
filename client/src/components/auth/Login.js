558cc3e10c7fe0c0f15ba55f855438ae69eb8961
Richard Mena <richardmf9425@yahoo.com>
2019-10-05 22:56:49 -0400

    completed login and signup components with styling and routing

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+	const [ formInfo, setFormInfo ] = useState({
+		email: '',
+		password: ''
+	});
+	const { email, password } = formInfo;
+	const onChange = (e) => setFormInfo({ ...formInfo, [e.target.name]: e.target.value });
+	const onSubmit = async (e) => {
+		e.preventDefault();
+
+		console.log('good');
+	};
 	return (
-		<div>
-			<h1>Login</h1>
-		</div>
+		<Fragment>
+			<div className="signup">
+				<h1 className="large text-primary">Log In</h1>
+				<p className="lead">
+					<i className="fas fa-user" /> Log Into Your Account
+				</p>
+				<form className="form" onSubmit={(e) => onSubmit(e)}>
+					<div className="form-group">
+						<input
+							type="email"
+							placeholder="Email Address"
+							name="email"
+							value={email}
+							onChange={(e) => onChange(e)}
+							required
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							placeholder="Password"
+							name="password"
+							minLength="6"
+							value={password}
+							onChange={(e) => onChange(e)}
+							required
+						/>
+					</div>
+
+					<input type="submit" className="btn btn-primary" value="Log In" />
+				</form>
+				<p className="my-1">
+					Don't have an account? <Link to="/signup">Create Account </Link>
+				</p>
+			</div>
+		</Fragment>
 	);
 };
 
--- client/src/components/auth/SignUp.js
+++ client/src/components/auth/SignUp.js
@@ -1,10 +1,83 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+	const [ formInfo, setFormInfo ] = useState({
+		name: '',
+		email: '',
+		password: '',
+		passwordConfirm: ''
+	});
+	const { name, email, password, passwordConfirm } = formInfo;
+	const onChange = (e) => setFormInfo({ ...formInfo, [e.target.name]: e.target.value });
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		if (password !== passwordConfirm) {
+			console.log('password do not match');
+		} else {
+			console.log('good');
+		}
+	};
 	return (
-		<div>
-			<h1>Sign Up</h1>
-		</div>
+		<Fragment>
+			<div className="signup">
+				<h1 className="large text-primary">Sign Up</h1>
+				<p className="lead">
+					<i className="fas fa-user" /> Create Your Account
+				</p>
+				<form className="form" onSubmit={(e) => onSubmit(e)}>
+					<div className="form-group">
+						<input
+							type="text"
+							placeholder="Name"
+							name="name"
+							value={name}
+							onChange={(e) => onChange(e)}
+							required
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="email"
+							placeholder="Email Address"
+							name="email"
+							value={email}
+							onChange={(e) => onChange(e)}
+							required
+						/>
+						<small className="form-text">
+							This site uses Gravatar so if you want a profile image, use a Gravatar email
+						</small>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							placeholder="Password"
+							name="password"
+							minLength="6"
+							value={password}
+							onChange={(e) => onChange(e)}
+							required
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							placeholder="Confirm Password"
+							name="passwordConfirm"
+							minLength="6"
+							value={passwordConfirm}
+							onChange={(e) => onChange(e)}
+							required
+						/>
+					</div>
+					<input type="submit" className="btn btn-primary" value="Sign Up" />
+				</form>
+				<p className="my-1">
+					Already have an account? <Link to="/login">Sign In</Link>
+				</p>
+			</div>
+		</Fragment>
 	);
 };
 
