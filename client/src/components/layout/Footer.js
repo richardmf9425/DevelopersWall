c6a4b5c4d95fcd07aefacaeadf4a127520a2ab58
Richard Mena <richardmf9425@yahoo.com>
2019-10-08 23:51:27 -0400

    add footer component(unstyled)

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const Footer = () => {
+	return (
+		<Fragment>
+			<div className="footer">
+				<p>footer </p>
+			</div>
+		</Fragment>
+	);
+};
+
+export default Footer;
