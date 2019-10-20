b4137ff3be91ac7d3fee9953b406f7bd664d7628
Richard Mena <richardmf9425@yahoo.com>
2019-10-20 18:25:38 -0400

    if user logged in, then /home will redirect to /dashboard

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,12 @@
 import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Landing = () => {
+const Landing = ({ isAuthenticated }) => {
+	if (isAuthenticated) {
+		return <Redirect to="/dashboard" />;
+	}
 	return (
 		<Fragment>
 			<section className="landing">
@@ -65,5 +70,10 @@ const Landing = () => {
 		</Fragment>
 	);
 };
-
-export default Landing;
+Landing.propTypes = {
+	isAuthenticated: PropTypes.bool
+};
+const mapStateToProps = (state) => ({
+	isAuthenticated: state.auth.isAuthenticated
+});
+export default connect(mapStateToProps)(Landing);
